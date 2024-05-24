cc65e722651acc19b6f2845a2a8abcc863cae76a
LanceKitL <98525004+LanceKitL@users.noreply.github.com>
2024-05-25 01:43:40 +0800

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import axios from 'axios'
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [anime, setAnime] = useState([])
+  const url = `https://api.jikan.moe/v4/anime`;
+
+  useEffect(() => {
+    try {
+      axios.get(url)
+        .then(res => {
+          console.log(res.data.data)
+          setAnime(res.data.data)
+        })
+    } catch (error) {
+      console.log(error)
+    }
+
+  }, [url])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3,1fr)', gap: "3rem", placeItems: 'center', }} className="container">
+        {anime.map((anime) => {
+          return (
+            <div className='card' key={anime.mal_id}>
+              <div className='card-image'>
+                <img src={anime.images.jpg.image_url} alt={anime.title} />
+              </div>
+              <div className='card-content'>
+                <p className='title'>{anime.title}</p>
+                <p>{anime.status}</p>
+                <p>{anime.year}</p>
+                <p>{anime.score}</p>
+                <ul>
+                  {anime.genres.map((genre) => {
+                    return (
+                      <li key={genre.mal_id}>{genre.name}</li>
+                    )
+                  })}
+                </ul>
+              </div>
+            </div>
+          )
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
